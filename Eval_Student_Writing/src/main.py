b8ae970bdf7737a9c2d7df47807aad48a0f4a826
Pilar Pineiro <pilar.pineiro@redstor.com>
2022-01-30 12:08:01 +0100

    adapt code from kaggle

--- Eval_Student_Writing/src/main.py
+++ Eval_Student_Writing/src/main.py
@@ -1,5 +1,112 @@
 
+import os
+
+if os.environ.get("KAGGLE_KERNEL_RUN_TYPE") is None:
+    ON_KAGGLE = False
+else:
+    ON_KAGGLE = True
+if not ON_KAGGLE:
+
+    for dirname, _, filenames in os.walk('../data/feedback-prize-2021/'):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
+
+    
+else:
+    for dirname, _, filenames in os.walk('/kaggle/input'):
+        for filename in filenames:
+            print(os.path.join(dirname, filename))
 
 
+class Config:
+    name = 'fp_exp3'
+    model_savename = 'longformer'
+    if ON_KAGGLE:
+        model_name = '../input/pt-longformer-base' # https://www.kaggle.com/kishalmandal/pt-longformer-base
+        base_dir = '/content/drive/MyDrive/petfinder'
+        data_dir = '../input/feedback-prize-2021/'
+        pre_data_dir = './preprocessed/'
+        model_dir = '.'
+        output_dir = '.'
+    else:
+        # customize for my own Google Colab Environment
+        model_name = '../model_base/longformer-base-4096' # download from Internet
+        base_dir = './'
+        data_dir = os.path.join(base_dir, 'data')
+        pre_data_dir = os.path.join(base_dir, 'data/preprocessed')
+        model_dir = os.path.join(base_dir, f'model/{name}')
+        output_dir = os.path.join(base_dir, f'output/{name}')
+    is_debug = False
+    n_epoch = 1 # not to exceed runtime limits on Kaggle
+    n_fold = 5
+    verbose_steps = 500
+    random_seed = 42
+    max_length = 1024
+    train_batch_size = 4
+    valid_batch_size = 4
+    lr = 5e-5
+    num_labels = 15
+    label_subtokens = True
+    output_hidden_states = True
+    hidden_dropout_prob = 0.1
+    layer_norm_eps = 1e-7
+    add_pooling_layer = False
+    verbose_steps = 500
+    if is_debug:
+        debug_sample = 1000
+        verbose_steps = 16
+        n_epoch = 1
+        n_fold = 2
 
-'/home/mapin/Documents/projects/personal_git/Kaggle/Kaggle/Eval_Student_Writing/data/feedback-prize-2021'
+IGNORE_INDEX = -100
+NON_LABEL = -1
+OUTPUT_LABELS = ['0', 'B-Lead', 'I-Lead', 'B-Position', 'I-Position', 'B-Claim', 'I-Claim', 'B-Counterclaim', 'I-Counterclaim', 
+                 'B-Rebuttal', 'I-Rebuttal', 'B-Evidence', 'I-Evidence', 'B-Concluding Statement', 'I-Concluding Statement']
+LABELS_TO_IDS = {v:k for k,v in enumerate(OUTPUT_LABELS)}
+IDS_TO_LABELS = {k:v for k,v in enumerate(OUTPUT_LABELS)}
+
+MIN_THRESH = {
+    "I-Lead": 9,
+    "I-Position": 5,
+    "I-Evidence": 14,
+    "I-Claim": 3,
+    "I-Concluding Statement": 11,
+    "I-Counterclaim": 6,
+    "I-Rebuttal": 4,
+}
+
+PROB_THRESH = {
+    "I-Lead": 0.7,
+    "I-Position": 0.55,
+    "I-Evidence": 0.65,
+    "I-Claim": 0.55,
+    "I-Concluding Statement": 0.7,
+    "I-Counterclaim": 0.5,
+    "I-Rebuttal": 0.55,
+}
+
+if not ON_KAGGLE:
+    if not os.path.exists(Config.model_dir):
+        !mkdir $Config.model_dir
+    if not os.path.exists(Config.output_dir):
+        !mkdir $Config.output_dir
+
+if not ON_KAGGLE:
+    !pip install -qq transformers
+
+# general
+import pandas as pd
+import numpy as np
+import random
+from tqdm.notebook import tqdm
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+import gc
+from collections import defaultdict
+# nlp
+from sklearn.feature_extraction.text import CountVectorizer
+import torch
+import torch.nn as nn
+from transformers import LongformerConfig, LongformerModel, LongformerTokenizerFast
+from torch.utils.data import Dataset, DataLoader
+from torch.cuda.amp import autocast, GradScaler
